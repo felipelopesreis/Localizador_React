474bdb7bee910a0bb051e01e7d452ef0c6ce841a
felipelopesreis <113152362+felipelopesreis@users.noreply.github.com>
2023-04-14 01:04:51 +0000

    Pesquisando-API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,8 +6,29 @@ import { CssBaseline } from '@mui/material'
 import { Base } from './Layout'
 import { Theme } from './Layout/ThemeProvider'
 import { PaginaPerfis } from './Paginas/PaginaPerfis'
+import {useQuery} from '@tanstack/react-query'
+import { getUser } from '../'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
+type user={
+  avatarURL: String,
+  htmlURL: String,
+  name: String
+}
 function App() {
+
+  const [search, setSearch] = useState("")
+  const inputRef = useRef
+  const handleClick = ()=>{
+    const username = inputRef.current
+  }
+  const {data, isLoading} = useQuery({
+    queryKey: ["user-github"],
+    queryFn: () =>{
+      getUser(search)
+    },  
+  }) 
+
  return(
   <>
   <Theme>
