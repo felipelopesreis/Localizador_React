91c4f57415cf7f6ae6d268aff5331190afc62814
felipelopesreis <113152362+felipelopesreis@users.noreply.github.com>
2023-04-20 02:43:10 +0000

    Finalizando busca com a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from 'react'
+import { FormEvent, useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, TextField, Button, Grid, Box, CircularProgress, Card, CardContent, Avatar, Typography } from '@mui/material'
 import { Base } from './Layout'
 import { Theme } from './Layout/ThemeProvider'
 import { PaginaPerfis } from './Paginas/PaginaPerfis'
@@ -17,7 +17,15 @@ type user={
 }
 function App() {
 
-  const [search, setSearch] = useState("")
+  const [user, setUser] = useState<User>({avatar_url:"",html_url:""});
+
+  const handleSubmit = (event:FormEvent<HTMLFormElement)=>{
+    event.preventDefault()
+    const form = event.currentTarget
+    const inputUserName:HTMLInputElement = form.userName
+  };
+  useEffect(()=>, [])
+
   const inputRef = useRef
   const handleClick = ()=>{
     const username = inputRef.current
@@ -37,7 +45,33 @@ function App() {
     <PaginaPerfis />
   </Base>
   </Theme>
+
+  <Grid item lg={6} sm={12} xs={12}>
+  <Box display='flex'>
+    <form onSubmit={handleSubmit}>
+      <TextField label='Nome do perfil' fullWidth name="userName"/>
+      <Button variant='contained' type='submit'>Buscar</Button>
+        <ConsultasPerfis/>
+     </form>   
+  </Box>
+  </Grid>
+  <Grid item lg={6} sm={12} xs={12}>
+  {isLoading ?(
+  <CircularProgress/>
+  ):(
+      <Card>
+        <CardContent>
+          <Avatar src={user.avatar_url}/>
+          <Typography variant='h4'>
+            {user.name}
+          </Typography>
+        </CardContent>
+      </Card>
+
+  )}
+  </Grid>
   </>
+  
  )
 }
 
